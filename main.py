3f33eb96aa69abe0fcac8dfa307f7c39aabbc050
Akash-Gattidi <akashgattidi2006@gmail.com>
2026-04-04 14:13:54 +0530

    Update main.py: match skills and calculate score using matcher.py

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ print("Extracted Text:\n")
 print(text)
 
 cleaned_text = clean_text(text)
+
+# Step 3: Define job required skills
+required_skills = ["Python", "Java", "Machine Learning", "SQL", "React"]
+
+# Step 4: Match skills
+matched_skills, score = match_skills(cleaned_text, required_skills)
+
+# Step 5: Output results
+print("\nMatched Skills:", matched_skills)
+print("Score:", score, "%")
